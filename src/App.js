b8425b87e80c864bbebc5929fae7c79468887e88
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-18 20:25:58 +0200

    Example 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
 import ErrorPage from "./pages/ErrorPage";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-
-        <Route component={ErrorPage} />
+        <Route path="/projects" component={ProjectsPage} />
       </Switch>
     </div>
   );
