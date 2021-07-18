077a207e34a3fccfa750bd17b29cba751ee53f6a
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-18 22:24:49 +0200

    Example 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";   // <== IMPORT
+import { Link, NavLink } from "react-router-dom";   // <== IMPORT NavLink
 
 function Navbar() {
   return (
     <nav className="Navbar">
       <ul>
-        <Link to="/"> Home </Link>                {/* <== ADD */}
-        <Link to="/about"> About </Link>             {/* <== ADD */}
-        <Link to="/projects"> Projects </Link>          {/* <== ADD */}
+        {/* <Link to="/"> Home </Link> */}
+        {/* <Link to="/about"> About </Link> */}
+        {/* <Link to="/projects"> Projects </Link> */}
+
+        {/*  ADD 👇 */}
+        <NavLink exact to="/" activeClassName="selected"> Home </NavLink>
+        <NavLink to="/about" activeClassName="selected"> About </NavLink>
+        <NavLink to="/projects" activeClassName="selected"> Projects </NavLink>
       </ul>
     </nav>
   );
