d4c166e4f5737b4fce5f11495d7a14ca163d9368
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-18 23:24:09 +0200

    Example 5

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import ProjectsPage from "./pages/ProjectsPage";
 import ErrorPage from "./pages/ErrorPage";
 import { Switch, Route } from "react-router-dom";
 
+import HomePageWithRedirect from "./pages/HomePageWithRedirect";  // <== IMPORT 
+
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        {/* <Route exact path="/" component={HomePage} /> */}
+        <Route exact path="/" component={HomePageWithRedirect} />  {/*   <== ADD   */}
+
         <Route path="/about" component={AboutPage} />
         <Route path="/projects" component={ProjectsPage} />
 
-        <Route component={ErrorPage} />                      {/*   <== ADD   */}
+        <Route component={ErrorPage} />
       </Switch>
     </div>
   );
