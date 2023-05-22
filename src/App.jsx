e439ead16308864b5c6728e5aac53b1f4ad5a139
ross-u <cirkovic.uros.srb@gmail.com>
2023-05-22 15:07:05 +0200

    refactor: update starter code to use vite react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ErrorPage from "./pages/ErrorPage";
+import HomePageWithNavigate from "./pages/HomePageWithNavigate";
+import { Routes, Route } from "react-router-dom";
+
+import projectsData from './projects-data.json';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+      <Routes>
+        {/* <Route  path="/" element={ <HomePage /> } /> */}
+
+        <Route path="/" element={ <HomePageWithNavigate /> } />
+        <Route path="/about" element={ <AboutPage /> } />
+        
+        <Route
+          path="/projects"
+          element={ <ProjectsPage projects={projectsData} /> }
+        />
+
+        <Route path="*" element={ <ErrorPage /> } />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
